e1e04c62252ee7ab39f5d466bc183cb3dfd3ae6d
Marton Gombocz <gomboczmarton@gmail.com>
2019-06-18 13:56:44 +0200

    init

---
+++ src/main/java/biz/gombocz/springbootkafka/KafkaMessageProducer.java
@@ -0,0 +1,31 @@
+package biz.gombocz.springbootkafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Component
+public class KafkaMessageProducer {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String msg) {
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("mytopic", msg);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+        @Override
+        public void onSuccess(SendResult<String, String> result) {
+            System.out.println("Sent message=[" + msg + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+        }
+
+        @Override
+        public void onFailure(Throwable ex) {
+            System.out.println("Unable to send message=[" + msg + "] due to : " + ex.getMessage());
+        }
+        });
+    }
+}
