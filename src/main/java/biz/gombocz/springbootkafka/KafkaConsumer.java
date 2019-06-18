ea547b9b7fc21acca6c4d55ded976fe4b72ca176
Marton Gombocz <gomboczmarton@gmail.com>
2019-06-18 15:36:46 +0200

    - Listen to Topic by Partition with Offset - Filter messages to listen

--- src/main/java/biz/gombocz/springbootkafka/KafkaConsumer.java
+++ src/main/java/biz/gombocz/springbootkafka/KafkaConsumer.java
@@ -1,6 +1,8 @@
 package biz.gombocz.springbootkafka;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -26,7 +28,42 @@ public class KafkaConsumer {
     public void listenWithHeaders(
             @Payload String message,
             @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
-        System.out.println(
-                "Received Message: " + message + "from partition: " + partition);
+        System.out.println("Received Message: " + message + "from partition: " + partition);
+    }
+
+    /**
+     * As you may have noticed, we had created the topic baeldung with only one partition. However, for a topic with multiple partitions,
+     * a @KafkaListener can explicitly subscribe to a particular partition of a topic with an initial offset:
+     */
+    @KafkaListener(
+            topicPartitions = @TopicPartition(topic = "topicName",
+                    partitionOffsets = {
+                            @PartitionOffset(partition = "0", initialOffset = "0"),
+                            @PartitionOffset(partition = "3", initialOffset = "0")
+                    }))
+    public void listenToPartition(
+            @Payload String message,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+        System.out.println("Received Messasge: " + message + "from partition: " + partition);
+    }
+
+    /**
+     * Since the initialOffset has been sent to 0 in this listener, all the previously consumed messages from partitions 0 and three will be re-consumed
+     * every time this listener is initialized. If setting the offset is not required,
+     * we can use the partitions property of @TopicPartition annotation to set only the partiKafkaListenerContainerFactoryKafkaListenerContainerFactorytions without the offset:
+     */
+    @KafkaListener(topicPartitions
+            = @TopicPartition(topic = "topicName", partitions = { "0", "1" }))
+    public void listenToPartitionWithoutOffset(
+            @Payload String message,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+        System.out.println("Received Messasge: " + message + "from partition: " + partition);
+    }
+
+    @KafkaListener(
+            topics = "topicName",
+            containerFactory = "filterKafkaListenerContainerFactory")
+    public void listenFiltered(String message) {
+        // handle message
     }
 }
--- src/main/java/biz/gombocz/springbootkafka/KafkaConsumerConfig.java
+++ src/main/java/biz/gombocz/springbootkafka/KafkaConsumerConfig.java
@@ -39,8 +39,26 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+            ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    /**
+     *
+     * Listeners can be configured to consume specific types of messages by adding a custom filter.
+     * This can be done by setting a RecordFilterStrategy to the KafkaListenerContainerFactory:
+     */
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String>
+    filterKafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, String> factory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setRecordFilterStrategy(
+                record -> record.value().contains("World"));
         return factory;
     }
 }
